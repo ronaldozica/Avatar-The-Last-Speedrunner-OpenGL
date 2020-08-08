5844e23b58a0184164c7407aced24743e67c3671
ronaldozica <rzica42@gmail.com>
2020-08-08 17:22:37 -0300

    Adicionados novos inimigos e sistema inicial de colisão.

---
+++ alfa_vx/alfa_v8/defines.h
@@ -0,0 +1,54 @@
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+	#define JPOSX 1000
+	#define JPOSY 1000
+
+	#define JTAMX 1000
+	#define JTAMY 1000
+
+	#define AQUILES_X 80
+	#define AQUILES_Y 350
+
+	#define ALTURA_PULO 550
+	#define INCREMENTO_PULO 18
+
+	#define ALTURA_MINIMA_CANCELA_PULO 400
+
+	#define AQUILES_LARG 100
+	#define AQUILES_ALT 100
+
+	#define PERIODO 33
+
+	#define VELOCIDADE_AQUILES 4
+	#define VELOCIDADE_FUNDO 4
+
+	#define DISTANCIA_FASE 1000
+
+	#define INIMIGOS_FASE_1 20
+
+	#define ARVORE_Y 380
+
+	#define ARVORE_LARG 100
+	#define ARVORE_ALT 135
+
+	#define ARBUSTO_Y 320
+
+	#define ARBUSTO_LARG 50
+	#define ARBUSTO_ALT 50
+
+	#define PEDRA_Y 320
+
+	#define PEDRA_LARG 65
+	#define PEDRA_ALT 50
+
+	#define VELOCIDADE_INIMIGO 10
+
+	#define X_MIN_INIMIGO 0
+	#define X_MAX_INIMIGO 100
+
+	#define OFFSET_ALTURA 350
+	#define OFFSET_LARGURA 10
+
+#endif
---
+++ alfa_vx/alfa_v8/inicializaVariaveis.c
@@ -0,0 +1,37 @@
+
+#include "includes.h"
+#include "variaveis.h"
+#include "defines.h"
+
+void inicializaVariaveis()
+{  
+    flag_aquiles = 1;
+    
+    quadro_aquiles[0] = 0.00;
+    quadro_aquiles[1] = 0.25;
+    quadro_aquiles[2] = 0;
+    quadro_aquiles[3] = 1;
+
+    cont_frames_aquiles = 0;
+
+    quadro_fundo[0] = 0;
+    quadro_fundo[1] = 1;
+    quadro_fundo[2] = 0;
+    quadro_fundo[3] = 1;
+
+    posicao_fundo = JPOSX;      // VAI VARIAR DE ACORDO COM O VALOR JPOSX!! CUIDADO
+
+    altura_aquiles = AQUILES_Y;
+
+    pulo = 0;
+
+    distancia = 0;
+
+    for(int cont = 0; cont < INIMIGOS_FASE_1; cont ++)
+    {
+        pos_inimigo[cont] = 0;
+        tipo_inimigo[cont] = rand() % 3;
+    }
+
+    contador_inimigos = 0;
+}
